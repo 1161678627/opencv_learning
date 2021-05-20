f812ba60b97ab39691840ccc8aa13c06758e17f6
1161678627 <1161678627@qq.com>
2021-05-20 12:35:06 +0800

    使用opencv自带的haar文件动态检测人脸和人眼

--- haar_cascade_object_detection.py
+++ haar_cascade_object_detection.py
@@ -18,9 +18,9 @@ while True:
     # print(faces)
     for x, y, w, h in faces:
         cv2.rectangle(img=frame, pt1=(x, y), pt2=(x+w, y+h), color=(0, 0, 255), thickness=2)
-        roi_gray = gray[x:x+w, y:y+h]
+        roi_gray = gray[y:y+h, x:x+w]   # 注意opencv在截取roi时，是先竖着截height再横着截width的
         # roi_color 是从 frame取出来的局部，对roi_color进行修改，如画矩形，frame 也会收到影响，在对应区域出现矩形的，不完全拷贝
-        roi_color = frame[x:x+w, y:y+h]
+        roi_color = frame[y:y+h, x:x+w]  # 注意opencv在截取roi时，是先竖着截height再横着截width的
         # 这里检测眼睛的思路是，从检测到的脸上进一步检测眼睛，提高眼睛检测的健壮性
         eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=1)
         for ex, ey, ew, eh in eyes:
